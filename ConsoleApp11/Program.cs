3d21e8432d734d2c2912b490263eb64b8a8b959d
user <user@526-3>
2022-09-14 11:22:00 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadLine(lang);
+            Console.WriteLine("Введите язык");
+            string? lang = Console.ReadLine();
+            //Console.ReadLine(lang);
+            //string lang = "ua";
+
+            
+
+            switch (lang)
+            {
+                case "ru":
+                    Console.WriteLine("Привет мир!");
+                    break;
+                case "en":
+                    Console.WriteLine("Hello world!");
+                    break;
+                case "ua":
+                    Console.WriteLine("Привiт народ!");
+                    break;
+                case "az":
+                    Console.WriteLine("Salam dunya!");
+                    break;
+                default:
+                    Console.WriteLine("Локализация не найдена!");
+                    break;
+
+            }
+        }
+    }
+}
